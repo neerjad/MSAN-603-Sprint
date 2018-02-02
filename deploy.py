cbee6604656f3ca22766fe48ed4f53bc66801828
neerjad <neerja.doshi@gmail.com>
2018-02-02 00:04:11 -0800

    Working code

---
+++ deploy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import paramiko
+import time
+
+## Example Deploy Script
+## This file uses paramiko to login to a box. Note that this is a skeleton file and you will need to do a bunch to complete the assignment.
+
+print "Connecting to box"
+ssh = paramiko.SSHClient()
+#k = paramiko.RSAKey.from_private_key_file("/Users/neerjadoshi/msan/BusinessStrategies/MSAN-603-Sprint/sprint.pem")
+
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('ec2-54-186-173-133.us-west-2.compute.amazonaws.com', username = 'ec2-user', key_filename = 'sprint.pem' )
+
+ssh.exec_command('sudo yum install git -y')
+#ssh.exec_command("rm -rf MSAN-603-Sprint; rm -rf Output; mkdir ~/Output; git clone https://github.com/neerjad/MSAN-603-Sprint.git; echo '1 * * * * python /home/ec2-user/MSAN-603-Sprint/hello_world.py' >  ~/.crontab; crontab ~/.crontab")
+ssh.exec_command('rm -rf MSAN-603-Sprint; rm -rf Output; mkdir ~/Output; git clone https://github.com/neerjad/MSAN-603-Sprint.git; crontab -l | echo "*/5 * * * * python /home/ec2-user/MSAN-603-Sprint/hello_world.py" | crontab -')
+
+print "Pull from Github successful"
+time.sleep(10)
+
+
+
+
+
+
+print "Script fully executed ... exiting" 
+ssh.close()
+## EOF ##
