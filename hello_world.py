b3c8c95ddce02675a1481c6a878c50e8d4b66893
neerjad <neerja.doshi@gmail.com>
2018-02-01 23:12:52 -0800

    Output

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 import time
 start_time = time.time()
 
-with open('/home/ec2-user/Output/output_'+'str(start_time)', 'w') as f:
+with open('~/Output/output_'+str(start_time), 'w') as f:
 	f.write('Hello World'+"\n")
-    f.write("Done!\n")
+	f.write("Done!\n")
