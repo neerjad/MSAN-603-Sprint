98004702b421e2dff28c086a91475a5bc8073605
Kishan <kpanchal7@berkeley.edu>
2018-02-25 11:01:31 -0800

    added function to get valid json from raw file but currently not working

--- flask_server.py
+++ flask_server.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from jinja2 import Environment
+import json  
+import os 
 
 application = Flask(__name__)
 
@@ -10,19 +12,41 @@ application = Flask(__name__)
 def home_page():
     return render_template('layout.html')
 
+  
+# check a file for valid json
+name = []
+age = []  
 
+def json_from_file(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            try:
+                data = json.loads(line)
+                if data.get('name') == None or data.get('name') == '':
+                    continue
+                if type(data['prop'].get('age')) == int and data['prop'].get('age') >= 0:
+                    name.append(data['name'])
+                    age.append(data['prop']['age'])
+            except:
+                continue
+        return 
+
+       
 @application.route('/json-example', methods=['POST']) 
 def json_example():
-    req_data = request.get_json()
-    # name = req_data['name']
-    # age = req_data['age']
-    # zipcode = req_data['prop']['zipcode'] 
-    # dmid = req_data['prop']['DMID']
-    # print '''name: {} age: {} zipcode: {} DMID: {}'''.format(name, age, zipcode, dmid)
+    req_data = request.get_json() 
     with open("Raw.txt", "a") as f: 
-        f.write(str(req_data).replace('\n','')+'\n') # append json to raw file 
-    return 'Your JSON file has been uploaded'
+        f.write(str(req_data).replace('\n','')+'\n') # append json to Raw.txt file 
+    
+    valid_json = json_from_file('Raw.txt') # append valid json to proc.txt file
+    with open('proc.txt', 'w') as f1:
+        for i in range(len(name)):
+            f1.write(str(name[i]) + '\t' + str(age[i]) + '\n')
 
+    return 'Your JSON file has been uploaded'
+      
+ 
 
 if __name__ == '__main__':
     application.run(host = '0.0.0.0', port = 8080, debug = True)
+
