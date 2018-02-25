d531b21d432671933dabf3906ed42102bd60106b
Kishan <kpanchal7@berkeley.edu>
2018-02-25 10:18:43 -0800

    remove web form, make raw file appendable in write

--- flask_server.py
+++ flask_server.py
@@ -10,26 +10,17 @@ application = Flask(__name__)
 def home_page():
     return render_template('layout.html')
 
-# @application.route("/", methods=['POST'])
-# # POST method to allow user to upload JSON
-# # https://stackoverflow.com/questions/20689195/flask-error-method-not-allowed-the-method-is-not-allowed-for-the-requested-url
-# def upload_data():
-#     input_data = request.get_json(force=True)
-#     # use request to get json   
-# 	# https://stackoverflow.com/questions/43126956/get-json-from-request-flask
-#     with open("Raw.txt", "wb") as f: f.write(str(input_data)) # write json to file 
-#     return 'Your JSON file has been uploaded'
-    # currently returning None instead of JSON ...  
 
 @application.route('/json-example', methods=['POST']) 
 def json_example():
     req_data = request.get_json()
-    name = req_data['name']
-    age = req_data['age']
-    zipcode = req_data['prop']['zipcode'] 
-    dmid = req_data['prop']['DMID']
-    print '''name: {} age: {} zipcode: {} DMID: {}'''.format(name, age, zipcode, dmid)
-    with open("Raw.txt", "wb") as f: f.write(str(req_data).replace('\n','')) # write json to file 
+    # name = req_data['name']
+    # age = req_data['age']
+    # zipcode = req_data['prop']['zipcode'] 
+    # dmid = req_data['prop']['DMID']
+    # print '''name: {} age: {} zipcode: {} DMID: {}'''.format(name, age, zipcode, dmid)
+    with open("Raw.txt", "a") as f: 
+        f.write(str(req_data).replace('\n','')+'\n') # append json to raw file 
     return 'Your JSON file has been uploaded'
 
 
